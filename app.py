9a6e74e2fe00473634b03fa6c8e201de5f583250
bum103103 <43300177+bum103103@users.noreply.github.com>
2023-05-16 11:18:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,11 @@ import streamlit as st
 
 x = st.slider('Select a value')
 st.write(x, 'squared is', x * x)
+
+st.title("this is the app title")
+st.markdown("this is the markdown")
+st.header("this is the header")
+st.subheader("this is the subheader")
+st.caption("this is the caption")
+st.code("x=2021")
+st.latex(r'''a_n=a*r^{n-1}+a*r^{n-2}+a*r^{n-3}+\dots+a*r^0''')
