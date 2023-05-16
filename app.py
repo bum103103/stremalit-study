3fd07a162151d31b86ead85178880bc278a23790
bum103103 <43300177+bum103103@users.noreply.github.com>
2023-05-16 11:34:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+rand=np.random.normal(1, 2, size=20)
+fig, ax = plt.subplots(1)
+ax.hist(rand, bins=15)
+st.pyplot(fig)
 
 x = st.slider('Select a value')
 st.write(x, 'squared is', x * x)
